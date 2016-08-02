1a492252069672b79edb6ecdb032fb3be2a0bcfe
kojisho <kojisho0729@gmail.com>
2016-08-02 18:14:51 +0900

    test2

--- mnist-captions/train0802.py
+++ mnist-captions/train0802.py
@@ -17,4 +17,21 @@ import math
 import cPickle as pickle
 from random import randint
 
+create_captions = __import__('create-captions')
+create_mnist_captions_dataset = create_captions.create_mnist_captions_dataset
+
+assert(theano.config.scan.allow_gc == True), "set scan.allow_gc to True ; otherwise you will run out of gpu memory"
+assert(theano.config.allow_gc == True), "set allow_gc to True ; otherwise you will run out of gpu memory"
+
+sys.stdout.flush()
+
+floatX = theano.config.floatX
+
+np.random.seed(np.random.randint(1 << 30))
+rng = RandomStreams(seed=np.random.randint(1 << 30))
+
+#np.random.seed(34)
+#rng = RandomStreams(seed=34)
+
+params_names = ['W_y_hLangEnc', 'W_hLangEnc_hLangEnc', 'b_hLangEnc', 'W_yRev_hLangEncRev', 'W_hLangEncRev_hLangEncRev', 'b_hLangEncRev', 'W_lang_align', 'W_hdec_align', 'b_align', 'v_align', 'W_s_hdec', 'W_hdec_read_attent', 'b_read_attent', 'W_henc_henc', 'W_inp_henc', 'b_henc', 'W_henc_mu', 'W_henc_logsigma', 'b_mu', 'b_logsigma', 'W_hdec_hdec', 'W_z_hdec', 'b_hdec', 'W_hdec_write_attent', 'b_write_attent', 'W_hdec_c', 'b_c', 'W_hdec_mu_and_logsigma_prior', 'b_mu_and_logsigma_prior', 'h0_lang', 'h0_enc', 'h0_dec', 'c0']
 
