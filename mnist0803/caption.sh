56d8e929a650605775d140de716d089994f62132
OGURA SHOTARO <gurao@rc3>
2016-08-08 14:20:43 +0900

    0808v2

---
+++ mnist0803/caption.sh
@@ -0,0 +1,1 @@
+THEANO_FLAGS='scan.allow_gc=1'  python sample-captions3.py --weights save/attention-vae-2016-8-6-22-54-54.h5 --sentence "the digit 1 is on the right of the digit 0 ."
---
+++ mnist0803/sample.sh
@@ -0,0 +1,1 @@
+THEANO_FLAGS='scan.allow_gc=1'  python sample-captions2.py --weights save/attention-vae-2016-8-6-22-54-54.h5
